4bb07bbe8364a3bbf8f50222723d627be34bd27a
Tejas <tejasvijay09@gmail.com>
2022-07-30 18:23:32 +0530

    Initial commit

---
+++ pallets/pallet-myown/src/lib.rs
@@ -0,0 +1,86 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/v3/runtime/frame>
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	// The pallet's runtime storage items.
+	// https://docs.substrate.io/v3/runtime/storage
+	#[pallet::storage]
+	#[pallet::getter(fn something)]
+	// Learn more about declaring storage items:
+	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
+	pub type AdditionValue<T> = StorageValue<_, u32>;
+
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/v3/runtime/events-and-errors
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// parameters. [something, who]
+		SomethingStored(u32, T::AccountId),
+		YesTheNumberHasBeenAdded(u32)
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Error names should be descriptive.
+		NoneValue,
+		/// Errors should have helpful documentation associated with them.
+		StorageOverflow,
+		FirstNumberCannotBeLessThanFifty,
+		SecondNumberCannotBeLessThanFifty
+	}
+
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn add_two_numbers(origin: OriginFor<T>, first_number: u32, second_number: u32) -> DispatchResult {
+			let signer = ensure_signed(origin)?;
+
+			ensure!(first_number > 50, <Error<T>>::FirstNumberCannotBeLessThanFifty);
+			ensure!(second_number > 50, <Error<T>>::SecondNumberCannotBeLessThanFifty);
+
+			let addition = first_number + second_number;
+
+			<AdditionValue<T>>::put(addition);
+
+			Self::deposit_event(Event::<T>::YesTheNumberHasBeenAdded(addition));
+
+			Ok(())
+		}
+
+	}
+}
